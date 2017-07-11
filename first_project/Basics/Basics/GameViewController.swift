5f701e5d64cffdf97959cf2d9f5d233f7ba6ac9a
yaburi <nathan.au@hotmail.com>
2017-07-11 21:45:48 +1000

    working on basics

--- first_project/Basics/Basics/GameViewController.swift
+++ first_project/Basics/Basics/GameViewController.swift
@@ -17,7 +17,7 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = MainMenu(fileNamed: "MainMenu") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
@@ -34,6 +34,19 @@ class GameViewController: UIViewController {
 
     override var shouldAutorotate: Bool {
         return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = MainMenu(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFit
+                
+                // Present the scene
+                view.presentScene(scene, transition: SKTransition.crossFade(withDuration: 1))
+            }
+        }
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
---
+++ first_project/Basics/Basics/MainMenu.swift
@@ -0,0 +1,15 @@
+//
+//  MainMenu.swift
+//  Basics
+//
+//  Created by Nathan Zhen on 9/7/17.
+//  Copyright © 2017 Nathan Zhen. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenu: SKScene {
+    override func didMove(to view: SKView) {
+        print("Custom class is running!")
+    }
+}
